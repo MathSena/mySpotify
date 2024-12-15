4b893e3a83003f786b08145e6d4b3f8b62a94b4d
MathSena <MathSena07@hotmail.com>
2024-12-14 23:23:44 -0300

    Atualiza o layout da página inicial para incluir instruções de login do Spotify e espaço para conteúdo adicional.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <section class="w-2/5 bg-green-500 absolute left-0 top-0 h-full p-20 flex flex-col justify-center items-center">
+    <h2 class="text-white text-4xl font-bold mt-20 text-center">
+      Entre com sua conta Spotify para ver as suas playlists favoritas!
+    </h2>
+  </section>
+  <section class="w-3/5 absolute right-0 top-0 h-full bg-gray-100 flex justify-center items-center">
+    <!-- Conteúdo adicional pode ser adicionado aqui -->
+  </section>
 </template>
